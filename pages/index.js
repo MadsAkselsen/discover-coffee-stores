f1d44c5513b872fea923900f45194c7727dd66e7
Mads Akselsen <mads.akselsen@maersk.com>
2023-02-25 20:20:57 +0100

    added prerendering static props

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,18 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "@/components/banner";
 import Card from "@/components/card";
-import coffeeStores from "../data/coffee-stores.json";
+import coffeeStoresData from "../data/coffee-stores.json";
 
 export async function getStaticProps(context) {
+	// logging only in terminal, not in dev tools in browser
+	console.log("hello from getStaticProps");
 	return {
-		props: { coffeeStores }, // will be passed to the page component as props
+		props: { coffeeStores: coffeeStoresData }, // will be passed to the page component as props
 	};
 }
 
 export default function Home(props) {
-	console.log("coffeeStores", coffeeStores);
+	console.log("coffeeStores props", coffeeStoresData);
 	const handleOnBannerBtnClick = () => {
 		console.log("hi banner button");
 	};
@@ -40,19 +42,24 @@ export default function Home(props) {
 						height={400}
 					/>
 				</div>
-				<div className={styles.cardLayout}>
-					{props.coffeeStores.map((coffeeStore) => {
-						return (
-							<Card
-								className={styles.card}
-								key={coffeeStore.id}
-								name={coffeeStore.name}
-								imgUrl={coffeeStore.imgUrl}
-								href={`/coffee-store/${coffeeStore.id}`}
-							/>
-						);
-					})}
-				</div>
+				{coffeeStoresData.length > 0 && (
+					<>
+						<h2 className={styles.heading2}>Toronto stores</h2>
+						<div className={styles.cardLayout}>
+							{props.coffeeStores.map((coffeeStore) => {
+								return (
+									<Card
+										className={styles.card}
+										key={coffeeStore.id}
+										name={coffeeStore.name}
+										imgUrl={coffeeStore.imgUrl}
+										href={`/coffee-store/${coffeeStore.id}`}
+									/>
+								);
+							})}
+						</div>
+					</>
+				)}
 			</main>
 		</div>
 	);
