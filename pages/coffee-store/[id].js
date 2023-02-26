2bba01a2b0b79dfaac6f9b15ce3f2be55fe6784d
Mads Akselsen <mads.akselsen@maersk.com>
2023-02-26 15:05:29 +0100

    added getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,15 +1,40 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export function getStaticProps(staticProps) {
+	console.log("testssss");
+	const params = staticProps.params;
+	return {
+		props: {
+			coffeeStore: coffeeStoresData.find((coffeeStore) => {
+				return coffeeStore.id.toString() === params.id;
+			}),
+		},
+	};
+}
+
+export function getStaticPaths() {
+	console.log("getStaticPaths");
+	return {
+		paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+		fallback: false, // can also be true or 'blocking'
+	};
+}
+
+const CoffeeStore = (props) => {
 	const router = useRouter();
-	console.log("router", router);
+
+	console.log("props", props);
 	return (
 		<div>
 			Coffee Store Page {router.query.id}
 			<Link href="/">Back to home</Link>
 			<Link href="/coffee-store/dynamic">Go to page dynamic</Link>
 			<Link href="/courses/nextjs">Go to page dynamic</Link>
+			<p>{props.coffeeStore.address}</p>
+			<p>{props.coffeeStore.name}</p>
 		</div>
 	);
 };
